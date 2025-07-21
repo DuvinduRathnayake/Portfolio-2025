89da2726481ac91993b33425ff57406a59956424
Duvindu Rathnayake <duvindurathnayake77@gmail.com>
2025-07-21 00:03:29 -0400

    Added header

--- duvindu-portfolio/src/App.jsx
+++ duvindu-portfolio/src/App.jsx
@@ -2,8 +2,6 @@ import ThemeToggle from './components/ThemeToggle';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className='min-h-screen flex items-center justify-center bg-slate-100 dark:bg-slate-900 text-slate-900 dark:text-white'>
       <ThemeToggle />
